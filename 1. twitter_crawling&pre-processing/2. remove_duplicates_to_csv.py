413307a8f8446e2762af2166001747b54166c152
xKwan <CLOW025@e.ntu.edu.sg>
2022-04-08 15:32:44 +0800

    crawling

--- 1. twitter_crawling&pre-processing/2. remove_duplicates_to_csv.py
+++
@@ -1,8 +0,0 @@
-# remove duplicate and convert to CSV file
-
-from twittercrawler.data_io import FileReader
-results_df = FileReader("stream_results.txt").read()
-
-dedup_df = results_df.drop_duplicates(subset=['text']).reset_index(drop=True)
-print(len(dedup_df))
-dedup_df.to_csv('data.csv')
